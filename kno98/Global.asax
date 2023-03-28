365209cbd0e5c435d74cc6b777ed419f33ef5e8a
BOLIVAR MENDOZA <bolitto@gmail.com>
2023-03-28 00:16:23 -0500

    Add project files.

---
+++ kno98/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="kno98.MvcApplication" Language="C#" %>
---
+++ kno98/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kno98.Startup))]
+namespace kno98
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
